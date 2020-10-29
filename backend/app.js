6d971f7c45d5800aed080654e6a9c4a232436b18
Soloman Garcia <soloman.garcia@gmail.com>
2020-10-29 11:51:06 -0600

    add post route

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json({ limit: "50mb" }));
+
+app.post("/api/stores", (req, res) => {
+  let dbStores = req.body;
+  res.send();
+});
+
 app.get("/", (req, res) => res.send("Hello World"));
 
 app.listen(port, () =>
